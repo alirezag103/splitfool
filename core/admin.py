ceec4721f6f96ae2409743a5a295a362bf609ae6
alireza <164399453+alirezag103@users.noreply.github.com>
2024-08-10 23:50:52 +0330

    Installing core app and registering some admin models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core import models
 
 # Register your models here.
+
+@admin.register(models.Group)
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Connection)
+class GroupAdmin(admin.ModelAdmin):
+    pass
